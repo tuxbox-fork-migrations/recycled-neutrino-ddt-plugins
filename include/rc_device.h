8ad0f15ca34ba7c1d43cf51950da37665cb9b5e3
TangoCash <eric@loxat.de>
2017-11-05 11:36:41 +0100

    add common include file

---
+++ include/rc_device.h
@@ -0,0 +1,13 @@
+#include <config.h>
+
+#if HAVE_COOL_HARDWARE
+#define RC_DEVICE "/dev/input/nevis_ir"
+
+#elif HAVE_DUCKBOX_HARDWARE
+#define RC_DEVICE "/dev/input/event0"
+#define RC_DEVICE_FALLBACK "/dev/input/event1"
+#else
+#define RC_DEVICE "/dev/input/event1"
+#define RC_DEVICE_FALLBACK "/dev/input/event0"
+
+#endif
