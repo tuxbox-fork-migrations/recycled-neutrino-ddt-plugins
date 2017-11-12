0396fafa34ae351d3ff1ad33f8bb5cda5ac87621
TangoCash <eric@loxat.de>
2017-11-12 16:00:34 +0100

    fix spark rc

--- include/rc_device.h
+++ include/rc_device.h
@@ -3,6 +3,10 @@
 #if HAVE_COOL_HARDWARE
 #define RC_DEVICE "/dev/input/nevis_ir"
 
+#elif HAVE_SPARK_HARDWARE
+#define RC_DEVICE "/dev/input/nevis_ir"
+#define RC_DEVICE_FALLBACK "/dev/input/event1"
+
 #elif HAVE_DUCKBOX_HARDWARE
 #define RC_DEVICE "/dev/input/event0"
 #define RC_DEVICE_FALLBACK "/dev/input/event1"
