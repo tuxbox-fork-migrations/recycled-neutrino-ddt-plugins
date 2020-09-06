058f97d70042d55174c36ca9b1cadc94c4a92d71
Frankenstone <dampf_acc@online.de>
2020-09-06 13:34:14 +0200

    tuxwetter: fixed build with gcc 10 -fno-common, thx max

--- tuxwetter/tuxwetter.c
+++ tuxwetter/tuxwetter.c
@@ -72,6 +72,26 @@ static char TCF_FILE[128]="";
 #define LCD_CPL 	12
 #define LCD_RDIST 	10
 
+FT_Error 		error;
+FT_Library		library;
+FTC_Manager		manager;
+FTC_SBitCache		cache;
+FTC_SBit		sbit;
+#if FREETYPE_MAJOR == 2 && FREETYPE_MINOR == 0
+FTC_Image_Desc		desc;
+#else
+FTC_ImageTypeRec	desc;
+#endif
+FT_Face			face;
+FT_UInt			prev_glyphindex;
+FT_Bool			use_kerning;
+
+struct fb_fix_screeninfo fix_screeninfo;
+struct fb_var_screeninfo var_screeninfo;
+
+int fb;
+int startx, starty, sx, ex, sy, ey, preset;
+
 #if defined(HAVE_SPARK_HARDWARE) || defined(HAVE_DUCKBOX_HARDWARE)
 int sync_blitter = 0;
 
