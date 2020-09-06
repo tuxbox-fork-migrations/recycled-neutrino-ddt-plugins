46ca3e94f9bec869730bf3be667f4540cf7ddd9a
Frankenstone <dampf_acc@online.de>
2020-09-06 11:00:49 +0200

    Revert "tuxwetter: fix gcc 10"
    This reverts commit fa4d9cd2c162ced9b7438e0278ffe98130047ee2.

--- tuxwetter/tuxwetter.h
+++ tuxwetter/tuxwetter.h
@@ -41,21 +41,22 @@ extern int sync_blitter;
 //#define WWEATHER
 #define BUFSIZE 	4095
 
+
 enum {LEFT, CENTER, RIGHT};
 
-static FT_Error 	error;
-static FT_Library	library;
-static FTC_Manager	manager;
-static FTC_SBitCache	cache;
-static FTC_SBit		sbit;
+FT_Error 		error;
+FT_Library		library;
+FTC_Manager		manager;
+FTC_SBitCache		cache;
+FTC_SBit		sbit;
 #if FREETYPE_MAJOR == 2 && FREETYPE_MINOR == 0
-static FTC_Image_Desc	desc;
+FTC_Image_Desc		desc;
 #else
-static FTC_ImageTypeRec	desc;
+FTC_ImageTypeRec	desc;
 #endif
-static FT_Face		face;
-static FT_UInt		prev_glyphindex;
-static FT_Bool		use_kerning;
+FT_Face			face;
+FT_UInt			prev_glyphindex;
+FT_Bool			use_kerning;
 
 // rc codes
 
@@ -64,59 +65,61 @@ static FT_Bool		use_kerning;
 #undef KEY_STOP
 #undef KEY_PLAY
 
-#define KEY_1			2
-#define KEY_2			3
-#define KEY_3			4
-#define KEY_4			5
-#define KEY_5			6
-#define KEY_6			7
-#define KEY_7			8
-#define KEY_8			9
-#define KEY_9			10
-#define KEY_BACKSPACE		14
-#define KEY_UP			103
-#define KEY_LEFT		105
-#define KEY_RIGHT		106
-#define KEY_DOWN		108
-#define KEY_MUTE		113
-#define KEY_VOLUMEDOWN		114
-#define KEY_VOLUMEUP		115
-#define KEY_POWER		116
-#define KEY_HELP		138
-#define KEY_HOME		102
-#define KEY_EXIT		174
-#define KEY_MENU		139
-#define KEY_PAGEUP		104
-#define KEY_PAGEDOWN		109
-#define KEY_OK			0x160
-#define KEY_RED			0x18e
-#define KEY_GREEN		0x18f
-#define KEY_YELLOW		0x190
-#define KEY_BLUE		0x191
+#define KEY_1			 		2
+#define KEY_2			 		3
+#define KEY_3			 		4
+#define KEY_4			 		5
+#define KEY_5			 		6
+#define KEY_6			 		7
+#define KEY_7			 		8
+#define KEY_8			 		9
+#define KEY_9					10
+#define KEY_BACKSPACE           14
+#define KEY_UP                  103
+#define KEY_LEFT                105
+#define KEY_RIGHT               106
+#define KEY_DOWN                108
+#define KEY_MUTE                113
+#define KEY_VOLUMEDOWN          114
+#define KEY_VOLUMEUP            115
+#define KEY_POWER               116
+#define KEY_HELP                138
+#define KEY_HOME                102
+#define KEY_EXIT				174
+#define KEY_MENU               	139
+#define KEY_PAGEUP              104
+#define KEY_PAGEDOWN            109
+#define KEY_OK           		0x160
+#define KEY_RED          		0x18e
+#define KEY_GREEN        		0x18f
+#define KEY_YELLOW       		0x190
+#define KEY_BLUE         		0x191
 
-#define KEY_TVR			0x179
-#define KEY_TTX			0x184
-#define KEY_COOL		0x1A1
-#define KEY_FAV			0x16C
-#define KEY_EPG			0x16D
-#define KEY_VF			0x175
+#define KEY_TVR					0x179
+#define KEY_TTX					0x184
+#define KEY_COOL				0x1A1
+#define KEY_FAV					0x16C
+#define KEY_EPG					0x16D
+#define KEY_VF					0x175
 
-#define KEY_SAT			0x17D
-#define KEY_SKIPP		0x197
-#define KEY_SKIPM		0x19C
-#define KEY_TS			0x167
-#define KEY_AUDIO		0x188
-#define KEY_REW			0x0A8
-#define KEY_FWD			0x09F
-#define KEY_HOLD		0x077
-#define KEY_REC			0x0A7
-#define KEY_STOP		0x080
-#define KEY_PLAY		0x0CF
+#define KEY_SAT					0x17D
+#define KEY_SKIPP				0x197
+#define KEY_SKIPM				0x19C
+#define KEY_TS					0x167
+#define KEY_AUDIO				0x188
+#define KEY_REW					0x0A8
+#define KEY_FWD					0x09F
+#define KEY_HOLD				0x077
+#define KEY_REC					0x0A7
+#define KEY_STOP				0x080
+#define KEY_PLAY				0x0CF
 
 //devs
-static int fb;
+
+int fb;
 
 //framebuffer stuff
+
 enum {
 	FILL,
 	GRID
@@ -145,7 +148,6 @@ enum {
 	CMCP3,
 	CSP0
 };
-
 #define TRANSP 0
 
 extern int FSIZE_BIG;
@@ -162,16 +164,13 @@ extern int OFFSET_MIN;
 extern uint32_t bgra[];
 extern int stride;
 extern uint32_t *lfb, *lbb;
+
 extern char *proxyadress, *proxyuserpwd;
+struct fb_fix_screeninfo fix_screeninfo;
+struct fb_var_screeninfo var_screeninfo;
 
+int startx, starty, sx, ex, sy, ey, preset, debounce, rblock;
 extern int instance;
-
-static struct fb_fix_screeninfo fix_screeninfo;
-static struct fb_var_screeninfo var_screeninfo;
-
-static int startx, starty, sx, ex, sy, ey, preset, debounce, rblock;
-
-
 int get_instance(void);
 void put_instance(int pval);
 int PaintWideString(int dy, const char *string, int sx, int sy, int maxwidth, int layout, int size, int color);
