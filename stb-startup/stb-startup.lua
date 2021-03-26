f4f75a740bc51ce603ff879f04d1ffc43b3f4d56
BPanther <bpanther_ts@hotmail.com>
2021-03-26 16:43:39 +0100

    update stb-startup 1.24 - now for hd51/h7/bre2ze4k and vuplus4k

--- stb-startup/stb-startup.lua
+++ stb-startup/stb-startup.lua
@@ -1,7 +1,10 @@
 -- The Tuxbox Copyright
 --
 -- Copyright 2018 Markus Volk, Sven Hoefer, Don de Deckelwech
--- Changes for Boxmode 12 by BPanther 17/Mar/2019
+-- STB-Startup for HD51/H7/BRE2ZE4K
+--
+-- Changed, now also for VU+ SOLO 4K, VU+ DUO 4K, VU+ DUO 4K SE, VU+ ULTIMO 4K, VU+ UNO 4K, VU+ UNO 4K SE and VU+ ZERO 4K
+-- by BPanther 26/Mar/2021
 --
 -- Redistribution and use in source and binary forms, with or without modification, 
 -- are permitted provided that the following conditions are met:
@@ -25,7 +28,8 @@
 -- authors and should not be interpreted as representing official policies, either expressed
 -- or implied, of the Tuxbox Project.
 
-caption = "STB-Startup"
+caption = "STB-Startup v1.24 - "
+bmbox = 0
 
 n = neutrino()
 fh = filehelpers.new()
@@ -35,9 +39,44 @@ bootfile = "/boot/STARTUP"
 
 for line in io.lines(bootfile) do
 	i, j = string.find(line, devbase)
-	bm = string.find(line, "boxmode")
 	current_root = tonumber(string.sub(line,j+1,j+2))
-	current_boxmode = string.sub(line,bm)
+end
+
+vuproc = assert(io.popen("cat /proc/stb/info/vumodel"))
+vumodel = vuproc:read('*line')
+vuproc:close()
+
+bmproc = assert(io.popen("cat /proc/stb/info/model"))
+boxmodel = bmproc:read('*line')
+bmproc:close()
+
+if vumodel == "solo4k" or vumodel == "uno4k" or vumodel == "uno4kse" or vumodel == "ultimo4k" then
+	root1 = 5
+	root2 = 7
+	root3 = 9
+	root4 = 11
+--	vumodel = "VU+ " .. vumodel
+elseif vumodel == "duo4k" or vumodel == "duo4kse" then
+	root1 = 10
+	root2 = 12
+	root3 = 14
+	root4 = 16
+--	vumodel = "VU+ " .. vumodel
+elseif vumodel == "zero4k" then
+	root1 = 8
+	root2 = 10
+	root3 = 12
+	root4 = 14
+--	vumodel = "VU+ " .. vumodel
+elseif boxmodel == "hd51" or boxmodel == "h7" or boxmodel == "bre2ze4k" then
+	root1 = 3
+	root2 = 5
+	root3 = 7
+	root4 = 9
+	vumodel = boxmodel
+	bmbox = 1
+else
+	return
 end
 
 locale = {}
@@ -47,7 +86,10 @@ locale["deutsch"] = {
 	choose_partition = "\n\nBitte wählen Sie die neue Startpartition aus.",
 	start_partition = "Auf die gewählte Partition umschalten ?",
 	reboot_partition = "Jetzt neustarten ?",
-	empty_partition = "Die gewählte Partition ist leer !"
+	empty_partition = "Die gewählte Partition ist leer !",
+	on = "ein",
+	off = "aus",
+	react = "Gewünschte Partition nach Umstellung neu aktivieren."
 }
 
 locale["english"] = {
@@ -55,7 +97,10 @@ locale["english"] = {
 	choose_partition = "\n\nPlease choose the new boot partition.",
 	start_partition = "Switch to the choosen partition ?",
 	reboot_partition = "Reboot now ?",
-	empty_partition = "The selected partition is empty !"
+	empty_partition = "The selected partition is empty !",
+	on = "on",
+	off = "off",
+	react = "Select partition again after changing boxmode."
 }
 
 function sleep (a)
@@ -80,20 +125,151 @@ function basename(str)
 	return name
 end
 
-function get_imagename(root, boxmode)
+function mount(dev,destination)
+	local provider = fh:readlink("/bin/mount")
+	fh:mkdir("/tmp/testmount/")
+	if (provider == nil) or not string.match(provider, "busybox") then
+		os.execute("mount -l " .. dev .. " " .. destination)
+	else
+		os.execute("mount " .. dev .. " " .. destination)
+	end
+end
+
+function umount(path)
+	local provider = fh:readlink("/bin/umount")
+	if (provider == nil) or not string.match(provider, "busybox") then
+		os.execute("umount -l " .. path)
+	else
+		os.execute("umount " .. path)
+	end
+	if not fh:exist("/tmp/testmount/tmp", "d") then
+		fh:rmdir("/tmp/testmount")
+	end
+end
+
+rtp = 0
+rr = 0
+function is_active(root)
+	rr = rr + 1
+	if (current_root == root) then
+		rtp = rr
+		active = " *"
+	else
+		active = ""
+	end
+	return active
+end
+
+function get_cfg_value(str)
+	for line in io.lines("/var/tuxbox/plugins/stb-startup.conf") do
+		if line:match(str .. "=") then
+			local i,j = string.find(line, str .. "=")
+			r = tonumber(string.sub(line, j+1, #line))
+		end
+	end
+	return r
+end
+
+function create_cfg()
+	file = io.open("/var/tuxbox/plugins/stb-startup.conf", "w")
+	file:write("boxmode_12=0", "\n")
+	file:close()
+end
+
+function write_cfg(k, v, str)
+	local a
+	if (v == on) then a = 1 else a = 0 end
+	local cfg_content = {}
+	for line in io.lines("/var/tuxbox/plugins/stb-startup.conf") do
+		if line:match(str .. "=") then
+			table.insert (cfg_content, (string.reverse(string.gsub(string.reverse(line), string.sub(string.reverse(line), 1, 1), a, 1))))
+		else
+			table.insert (cfg_content, line)
+		end
+	end
+	file = io.open("/var/tuxbox/plugins/stb-startup.conf", 'w')
+	for i, v in ipairs(cfg_content) do
+		file:write(v, "\n")
+	end
+	io.close(file)
+end
+
+function set(k, v, str)
+	write_cfg(k, v, "boxmode_12")
+end
+
+if (bmbox == 1) and not fh:exist("/var/tuxbox/plugins/stb-startup.conf", "f") then
+	create_cfg()
+end
+
+function get_imagename(root)
 	imagename = ""
+	imgversion= ""
 	local glob = require "posix".glob
-	for _, j in pairs(glob('/boot/*', 0)) do
-		for line in io.lines(j) do
-			if (j ~= bootfile) then
-				if line:match(devbase .. root) then
-					if line:match(boxmode) then
+	local fh = filehelpers.new()
+	mount("/dev/mmcblk0p" .. root, "/tmp/testmount")
+	if fh:exist("/tmp/testmount/etc/image-version", "f") then
+		for line in io.lines("/tmp/testmount/etc/image-version") do
+			if line:match("distro" .. "=") then
+				local i,j = string.find(line, "distro" .. "=")
+				imagename = string.sub(line, j+1, #line)
+			end
+			if line:match("imageversion" .. "=") then
+				local i,j = string.find(line, "imageversion" .. "=")
+				imagename = imagename .. " " .. string.sub(line, j+1, #line)
+			end
+			if line:match("version" .. "=") then
+				local i,j = string.find(line, "version" .. "=")
+				imgversion = string.sub(line, j+2, j+3)
+				imgversion = imgversion .. "." .. string.sub(line, j+4, j+4)
+				imgversion = imgversion .. "." .. string.sub(line, j+5, j+5)
+			end
+			if imagename == "" and line:match("creator" .. "=") then
+				local i,j = string.find(line, "creator" .. "=")
+				if line == "creator=VTi <info@vuplus-support.org>" then
+					line = "creator=VTi"
+				end
+				imagename = string.sub(line, j+1, #line)
+			end
+			if imagename == "VTi" and imgversion ~= "" then
+				imagename = imagename .. " " .. imgversion
+			end
+		end
+	end
+	if imagename == "" and fh:exist("/tmp/testmount/.version", "f") then
+		for line in io.lines("/tmp/testmount/.version") do
+			if line:match("creator" .. "=") then
+				local i,j = string.find(line, "creator" .. "=")
+				imagename = string.sub(line, j+1, #line)
+			end
+			if line:match("git" .. "=") then
+				local i,j = string.find(line, "git" .. "=")
+				imagename = imagename .. " " .. string.sub(line, j+1, #line)
+			end
+		end
+	end
+	if imagename == "" then
+		for _, j in pairs(glob('/boot/*', 0)) do
+			for line in io.lines(j) do
+				if (j ~= bootfile) then
+					if bmbox == 1 then
+						if get_cfg_value("boxmode_12") == 1 then
+							boxmode = "boxmode=12'"
+						else
+							boxmode = "boxmode=1'"
+						end
+						if line:match(devbase .. root) and (j ~= nil) and line:match(boxmode) and not line:match("android") then
+							imagename = basename(j)
+						end
+					elseif line:match(devbase .. root) and (j ~= nil) and not line:match("android") then
 						imagename = basename(j)
 					end
 				end
 			end
 		end
+		if imagename == "" then imagename = "???" end
 	end
+	umount("/tmp/testmount")
 	return imagename
 end
 
@@ -103,57 +279,49 @@ lang = neutrino_conf:getString("language", "english")
 if locale[lang] == nil then
 	lang = "english"
 end
+on = locale[lang].on
+off = locale[lang].off
 timing_menu = neutrino_conf:getString("timing.menu", "0")
 
-cdx = 700;
-
-if (get_imagename(3, "boxmode=12'")) then
-	btn_6 = get_imagename(3, "boxmode=12'")
-	if (not (btn_6 == "")) then
-		cdx = cdx + 118
-	end
+if bmbox == 1 then
+	chooser_dx = n:scale2Res(900)
+else
+	chooser_dx = n:scale2Res(700)
 end
-if (get_imagename(5, "boxmode=12'")) then
-	btn_7 = get_imagename(5, "boxmode=12'")
-	if (not (btn_7 == "")) then
-		cdx = cdx + 118
-	end
-end
-if (get_imagename(7, "boxmode=12'")) then
-	btn_8 = get_imagename(7, "boxmode=12'")
-	if (not (btn_8 == "")) then
-		cdx = cdx + 118
-	end
-end
-if (get_imagename(9, "boxmode=12'")) then
-	btn_9 = get_imagename(9, "boxmode=12'")
-	if (not (btn_9 == "")) then
-		cdx = cdx + 118
-	end
-end
-
-chooser_dx = n:scale2Res(cdx)
 chooser_dy = n:scale2Res(200)
 chooser_x = SCREEN.OFF_X + (((SCREEN.END_X - SCREEN.OFF_X) - chooser_dx) / 2)
 chooser_y = SCREEN.OFF_Y + (((SCREEN.END_Y - SCREEN.OFF_Y) - chooser_dy) / 2)
 
-chooser = cwindow.new {
-	x = chooser_x,
-	y = chooser_y,
-	dx = chooser_dx,
-	dy = chooser_dy,
-	title = caption,
-	icon = "settings",
-	has_shadow = true,
-	btnRed = get_imagename(3, "boxmode=1'"),
-	btnGreen = get_imagename(5, "boxmode=1'"),
-	btnYellow = get_imagename(7, "boxmode=1'"),
-	btnBlue = get_imagename(9, "boxmode=1'"),
-	btn6 = btn_6,
-	btn7 = btn_7,
-	btn8 = btn_8,
-	btn9 = btn_9
-}
+if bmbox == 1 then
+	chooser = cwindow.new {
+		x = chooser_x,
+		y = chooser_y,
+		dx = chooser_dx,
+		dy = chooser_dy,
+		title = caption .. vumodel:upper(),
+		icon = "settings",
+		has_shadow = true,
+		btnRed = get_imagename(root1) .. is_active(root1),
+		btnGreen = get_imagename(root2) .. is_active(root2),
+		btnYellow = get_imagename(root3) .. is_active(root3),
+		btnBlue = get_imagename(root4) .. is_active(root4),
+		btnSetup = "Boxmode"
+	}
+else
+	chooser = cwindow.new {
+		x = chooser_x,
+		y = chooser_y,
+		dx = chooser_dx,
+		dy = chooser_dy,
+		title = caption .. vumodel:upper(),
+		icon = "settings",
+		has_shadow = true,
+		btnRed = get_imagename(root1) .. is_active(root1),
+		btnGreen = get_imagename(root2) .. is_active(root2),
+		btnYellow = get_imagename(root3) .. is_active(root3),
+		btnBlue = get_imagename(root4) .. is_active(root4)
+	}
+end
 
 chooser_text = ctext.new {
 	parent = chooser,
@@ -161,9 +329,9 @@ chooser_text = ctext.new {
 	y = OFFSET.INNER_SMALL,
 	dx = chooser_dx - 2*OFFSET.INNER_MID,
 	dy = chooser_dy - chooser:headerHeight() - chooser:footerHeight() - 2*OFFSET.INNER_SMALL,
-	text = locale[lang].current_boot_partition .. get_imagename(current_root, current_boxmode) .. locale[lang].choose_partition,
+	text = locale[lang].current_boot_partition .. rtp .. "\n" .. get_imagename(current_root) .. locale[lang].choose_partition,
 	font_text = FONT.MENU,
-	mode = "ALIGN_CENTER"
+	text_mode = "ALIGN_CENTER"
 }
 
 chooser:paint()
@@ -180,41 +348,56 @@ repeat
 	i = i + 1
 	msg, data = n:GetInput(d)
 	if (msg == RC['red']) then
-		root = 3
-		box_mode = "boxmode=1'"
+		root = root1
+		rootnum = 1
 		colorkey = true
 	elseif (msg == RC['green']) then
-		root = 5
-		box_mode = "boxmode=1'"
+		root = root2
+		rootnum = 2
 		colorkey = true
 	elseif (msg == RC['yellow']) then
-		root = 7
-		box_mode = "boxmode=1'"
+		root = root3
+		rootnum = 3
 		colorkey = true
 	elseif (msg == RC['blue']) then
-		root = 9
-		box_mode = "boxmode=1'"
+		root = root4
+		rootnum = 4
 		colorkey = true
-	elseif (msg == RC['6']) then
-		root = 3
-		box_mode = "boxmode=12'"
-		colorkey = true
-	elseif (msg == RC['7']) then
-		root = 5
-		box_mode = "boxmode=12'"
-		colorkey = true
-	elseif (msg == RC['8']) then
-		root = 7
-		box_mode = "boxmode=12'"
-		colorkey = true
-	elseif (msg == RC['9']) then
-		root = 9
-		box_mode = "boxmode=12'"
-		colorkey = true
+	elseif (bmbox == 1) and (msg == RC['setup']) then
+		chooser:hide()
+		menu = menu.new{icon="settings", name=locale[lang].options}
+		menu:addItem{type="back"}
+		menu:addItem{type="separatorline"}
+		if (get_cfg_value("boxmode_12") == 1) then
+			menu:addItem{type="chooser", action="set", options={on, off}, directkey=RC["setup"], name="Boxmode 12"}
+		elseif (get_cfg_value("boxmode_12") == 0) then
+			menu:addItem{type="chooser", action="set", options={off, on}, directkey=RC["setup"], name="Boxmode 12"}
+		end
+		menu:addItem{type="separatorline", name=locale[lang].react}
+		menu:exec()
+		chooser:paint()
 	end
 until msg == RC['home'] or colorkey or i == t
 
 chooser:hide()
+
+function make_cmdline(boxname, rn, rp)
+	if boxname == "ultimo4k" then cmdline = "boot emmcflash0.kernel_" .. rn .. " 'root=/dev/mmcblk0p" .. rp .. " rootfstype=ext4 rootflags=data=journal rootwait rw coherent_pool=2M vmalloc=622m bmem=630m@394m bmem=383m@1665m bmem=443m@2625m'\n"
+	elseif boxname == "duo4kse" then cmdline = "boot flash0.kernel_" .. rn ..  " 'libata.force=1:3.0G,2:3.0G,3:3.0G root=/dev/mmcblk0p" .. rp .. " rootfstype=ext4 rootflags=data=journal rootwait rw coherent_pool=2M vmalloc=622m bmem=630m@394m bmem=383m@1665m bmem=443m@2625m'\n"
+	elseif boxname == "duo4k" then cmdline = "boot flash0.kernel_" .. rn ..   " 'root=/dev/mmcblk0p" .. rp .. " rootfstype=ext4 rootflags=data=journal rootwait rw coherent_pool=2M vmalloc=500m bmem=891m@1156m bmem=575m@3520m'\n"
+	elseif boxname == "solo4k" then cmdline = "boot emmcflash0.kernel_" .. rn ..  " 'root=/dev/mmcblk0p" .. rp .. " rw rootwait rootflags=data=journal debug coherent_pool=2M brcm_cma=504M@0x10000000 brcm_cma=260M@0x2f800000 brcm_cma=1024M@0x80000000'\n"
+	elseif boxname == "uno4k" then cmdline = "boot emmcflash0.kernel_" .. rn ..  " 'root=/dev/mmcblk0p" .. rp .. " rootfstype=ext4 rootflags=data=journal rootwait rw coherent_pool=2M vmalloc=633m bmem=637m@383m bmem=637m@2431m'\n"
+	elseif boxname == "uno4kse" then cmdline = "boot emmcflash0.kernel_" .. rn ..  " 'root=/dev/mmcblk0p" .. rp .. " rootfstype=ext4 rootflags=data=journal rootwait rw coherent_pool=2M vmalloc=666m bmem=641m@350m bmem=641m@2430m'\n"
+	elseif boxname == "zero4k" then cmdline = "boot flash0.kernel_" .. rn ..  " 'root=/dev/mmcblk0p" .. rp .. " rootfstype=ext4 rootflags=data=journal rootwait rw coherent_pool=2M rw bmem=699m@1317m'\n"
+	elseif boxname == "hd51" or boxname == "h7" or boxname == "bre2ze4k" then
+		if get_cfg_value("boxmode_12") == 1 then
+			cmdline = "boot emmcflash0.kernel" .. rn ..  " 'brcm_cma=520M@248M brcm_cma=192M@768M root=/dev/mmcblk0p" .. rp .. " rw rootwait " .. boxname .. "_4.boxmode=12'\n"
+		else
+			cmdline = "boot emmcflash0.kernel" .. rn ..  " 'brcm_cma=440M@328M brcm_cma=192M@768M root=/dev/mmcblk0p" .. rp .. " rw rootwait " .. boxname .. "_4.boxmode=1'\n"
+		end
+	end
+	return cmdline
+end
 
 if colorkey then
 	local file = assert(io.popen("blkid " .. devbase .. root .. " | grep TYPE"))
@@ -232,16 +415,17 @@ if colorkey then
 		a,b,c = os.execute("test -d " .. mounted_part .. "/usr")
 	end
 	if (c == 1) then
-		local ret = hintbox.new { title = caption, icon = "settings", text = locale[lang].empty_partition };
+		local ret = hintbox.new { title = caption .. vumodel:upper(), icon = "settings", text = locale[lang].empty_partition };
 		ret:paint();
 		sleep(3)
 		return
 	else
 		res = messagebox.exec {
-			title = caption,
+			title = caption .. vumodel:upper(),
 			icon = "settings",
 			text = locale[lang].start_partition,
 			timeout = 0,
+			width = 475,
 			buttons={ "yes", "no" },
 			default = "no"
 		}
@@ -249,25 +433,15 @@ if colorkey then
 end
 
 if res == "yes" then
-	local glob = require "posix".glob
-	for _, j in pairs(glob('/boot/*', 0)) do
-		for line in io.lines(j) do
-			if line:match(devbase .. root) then
-				if line:match(box_mode) then
-					if (j ~= bootfile) then
-						local file = io.open(bootfile, "w")
-						file:write(line .. "\n")
-						file:close()
-					end
-				end
-			end
-		end
-	end
+	local file = io.open(bootfile, "w")
+	file:write(make_cmdline(vumodel, rootnum, root))
+	file:close()
 	res = messagebox.exec {
-		title = caption,
+		title = caption .. vumodel:upper(),
 		icon = "settings",
 		text = locale[lang].reboot_partition,
 		timeout = 0,
+		width = 475,
 		buttons={ "yes", "no" },
 		default = "no"
 	}
